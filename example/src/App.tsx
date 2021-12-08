97ea27aa5a549b0758df3f38dcf6e0421b3352de
tonyjahim <tonyjahim@gmail.com>
2021-12-08 09:32:16 +0100

    first commit

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,10 +1,61 @@
 import React from 'react'
 
-import { ExampleComponent } from 'use-message-state'
+import { useMessageState } from 'use-message-state'
 import 'use-message-state/dist/index.css'
 
+const A = () => {
+  const [firstname, setFirstname] = useMessageState('firstname', 'joe')
+  const [lastname, setLastname] = useMessageState('lastname', 'lavabo')
+
+  return (
+    <div>
+      <div>
+        Firstname :{' '}
+        <input
+          value={firstname}
+          onChange={(e) => {
+            setFirstname(e.target.value)
+          }}
+        />
+      </div>
+
+      <div>
+        Lastname :{' '}
+        <input
+          value={lastname}
+          onChange={(e) => {
+            setLastname(e.target.value)
+          }}
+        />
+      </div>
+    </div>
+  )
+}
+
+const B = () => {
+  const [firstname] = useMessageState('firstname', 'joe')
+  const [lastname] = useMessageState('lastname', 'lavabo')
+
+  return (
+    <div>
+      <div>Firstname : {firstname}</div>
+      <div>Lastname: {lastname}</div>
+    </div>
+  )
+}
+
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return (
+    <div className='App'>
+      <div>
+        <A />
+
+        <hr style={{ margin: '20px 0' }} />
+
+        <B />
+      </div>
+    </div>
+  )
 }
 
 export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,58 @@
 import * as React from 'react'
-import styles from './styles.module.css'
+import uniqid from 'uniqid'
 
-interface Props {
-  text: string
-}
+export function useMessageState<TData = any>(
+  canal: string,
+  defaultData: TData
+): [TData, React.Dispatch<React.SetStateAction<TData>>] {
+  const instanceIdRef = React.useRef(uniqid())
 
-export const ExampleComponent = ({ text }: Props) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+  const [state, _setState] = React.useState<TData>(defaultData)
+
+  const handleMessage = (e: any) => {
+    if (
+      e.data?.type === 'use-message-state' &&
+      e.data.from !== instanceIdRef.current &&
+      e.data.canal === canal
+    ) {
+      _setState(e.data.data)
+    }
+  }
+
+  const sendMessage = (data: TData) => {
+    window.postMessage(
+      {
+        type: 'use-message-state',
+        canal,
+        data,
+        from: instanceIdRef.current
+      },
+      document.location.origin
+    )
+  }
+
+  React.useEffect(() => {
+    window.addEventListener('message', handleMessage)
+
+    return () => {
+      window.removeEventListener('message', handleMessage)
+    }
+  }, [])
+
+  const setState = (newState: TData) => {
+    if (typeof newState === 'function') {
+      _setState((state) => {
+        const result = newState(state)
+
+        sendMessage(result)
+
+        return result
+      })
+    } else {
+      sendMessage(newState)
+      _setState(newState)
+    }
+  }
+
+  return [state, setState]
 }
